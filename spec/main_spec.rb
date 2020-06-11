8f2289cdf4211ceb215e143dbb7bcd511767351b
Laguna1 <lagunazoomarket@gmail.com>
2020-06-11 18:58:30 +0300

    added player_test.rb

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -37,15 +37,4 @@ describe Tictactoe do
 
 end
 
-describe Player do
-  describe '#initialize' do
-    it 'confirms new players game' do
-      player = Player.new('mike', 'X')
-      expect(player.instance_variable_get(:@name)).to eq('mike')
-    end
-    it 'confirms new players game' do
-      player = Player.new('mike', 'X')
-      expect(player.instance_variable_get(:@move)).to eq('X')
-    end
-  end
-end
+
